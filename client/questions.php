e1d9415337420181deca8b81229cab8c2630d111
Sunny-2610 <sunnysinha9884@gmail.com>
2025-11-26 14:52:08 +0530

    bug fix 2

--- client/questions.php
+++ client/questions.php
@@ -1,10 +1,17 @@
 <div class="container">
 
+    <?php 
+    // FIX: $uid was undefined on pages like Home, Latest, Category, Search.
+    // So we define it safely to avoid "Undefined variable $uid" warnings.
+    if (!isset($uid)) $uid = null;  
+    ?>
+
     <div class="row">
         <div class="col-8">
             <h1 class="heading">Questions</h1>
             <?php
             include("./common/db.php");
+
             if (isset($_GET["c-id"])) {
                 $query = "select * from questions where category_id=$cid";
             } else if (isset($_GET["u-id"])) {
@@ -12,21 +19,30 @@
             } else if (isset($_GET["latest"])) {
                 $query = "select * from questions order by id desc";
             } else if (isset($_GET["search"])) {
-                 $query = "select * from questions where `title` LIKE '%$search%' ";
-            }else {
+                $query = "select * from questions where `title` LIKE '%$search%'";
+            } else {
                 $query = "select * from questions";
             }
+
             $result = $conn->query($query);
+
             foreach ($result as $row) {
                 $title = $row['title'];
                 $id = $row['id'];
+
                 echo "<div class='row question-list'>
-    <h4 class='my-question'><a href='?q-id=$id'>$title</a>";
-    echo $uid?"<a href='./server/requests.php?delete=$id'>Delete</a>":NULL;
-    echo "</h4></div>";
+                        <h4 class='my-question'>
+                            <a href='?q-id=$id'>$title</a>";
+
+                // Show delete link only when $uid exists (My Questions page)
+                echo $uid ? "<a href='./server/requests.php?delete=$id'>Delete</a>" : NULL;
+
+                echo "   </h4>
+                      </div>";
             }
             ?>
         </div>
+
         <div class="col-4">
             <?php include('categorylist.php'); ?>
         </div>
